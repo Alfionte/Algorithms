fb8f923ce3bb4b79b4b5eae939f21d2aa4947578
Gabriele Porcelli <g.porcelli@thebeat.co>
2022-10-20 17:08:52 +0100

    Add Breadth first search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
-import binarysearch.binarySearch
-import binarysearch.binarySearchImproved
-import quicksort.improvedQuickSort
+import breadth.first.search.PersonNode
+import breadth.first.search.bfs
+import breadth.first.search.graph
 
 fun main() {
-
+    /*
     val unsortedList = listOf(0, 1, 2, 4, 5, 9, 12, 3, 56, 79, 88, 96, 111)
     val sortedImprovedList = improvedQuickSort(unsortedList)
     val binarySearch = binarySearch(sortedImprovedList, 111)
@@ -13,4 +13,8 @@ fun main() {
     println("Sorted list: $sortedImprovedList")
     println("Binary search : $binarySearch")
     println("Binary search improved: $binarySearchImproved")
+     */
+
+    val seller = bfs(PersonNode("Gabriele"), graph)
+    println(seller)
 }

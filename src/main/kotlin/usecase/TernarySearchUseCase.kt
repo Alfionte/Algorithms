4a240c1c918656fcac555a818009d82c2d2f0aa8
Gabriele Porcelli <g.porcelli@thebeat.co>
2022-10-27 15:13:21 +0100

    Add ternary comparison

--- src/main/kotlin/usecase/TernarySearchUseCase.kt
+++ src/main/kotlin/usecase/TernarySearchUseCase.kt
@@ -1,6 +1,7 @@
 package usecase
 
 import binarysearch.binarySearch
+import binarysearch.binarySearchImproved
 import ternarysearch.ternarySearch
 
 object TernarySearchUseCase : UseCase {
@@ -9,10 +10,12 @@ object TernarySearchUseCase : UseCase {
         println("--- TernarySearchUseCase ---")
 
         val list = (0..10320300).toList()
-        val binarySearch = binarySearch(list, 111)
-        val ternarySearch = ternarySearch(list, 111, 0)
+        val binarySearch = binarySearch(list, 10320299)
+        val binarySearchImproved = binarySearchImproved(list, 10320299)
+        val ternarySearch = ternarySearch(list, 10320299, 0)
 
         println("Binary search : found: ${binarySearch.first}, iterations: ${binarySearch.second}")
+        println("Binary search improved (best case) : found: ${binarySearchImproved.first}, iterations: ${binarySearchImproved.second}")
         println("Ternary search : found: ${ternarySearch.first}, iterations: ${ternarySearch.second}")
         println()
     }
